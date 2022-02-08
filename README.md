50dfbc92f4930c408b69a9409feea3d348d0a09d
Nitro Sniper <44097331+NitroSniper@users.noreply.github.com>
2022-02-08 21:05:23 +0000

    Add files via upload

---
+++ README.md
@@ -0,0 +1,13 @@
+# DadJokeAndEmailBot
+Making a Bot that emails a Dad Joke to a Target of Choosing. \n
+Reason - Want to prank a school friend. \n
+DateStarted - 17/03/2021. \n
+
+
+REQUIREMENT - Sender has to be GMAIL (I think)
+
+
+Just mainly me having fun. if you want to use the code. just message me saying it and I'll most likely be fine
+
+If you are using the code you have to change User Agent to the repo/website you are placing your code in. 
+
--- main.py
+++ main.py
@@ -1,6 +1,39 @@
 import requests
 import json
 
-APISite = https://icanhazdadjoke.com/
+APISite = 'https://icanhazdadjoke.com/'
 userAgent = 'Mozilla/5.0'
-response = requests.get()
+
+headers = {
+    'Accept': 'text/plain',
+    'User-Agent': 'My Libary (https://github.com/NitroSniper/DadJokeAndEmailer)'
+}   
+response = requests.get(headers=headers, url=APISite)
+dadJoke = response.content.decode('utf-8')
+
+
+senderEmail = '' #Enter the email of the sender you are gonna use. make sure it is gmail
+receiverEmails = [] #enter the name of the recipient in a string.
+
+
+message = '''\
+Subject:DadJoke
+%s
+
+By Ortin Fargo >:D
+
+- NoMercy 
+''' % dadJoke
+
+import smtplib, ssl
+
+port = 465  # For SSL
+password = #NOTHING TO SEE HERE LDSAJFLASDJFLJSLDFKLSDFJKL
+
+# Create a secure SSL context
+context = ssl.create_default_context()
+
+with smtplib.SMTP_SSL("smtp.gmail.com", port, context=context) as server:
+    server.login(senderEmail, password)
+    for receiverEmail in receiverEmails:
+        server.sendmail(senderEmail, receiverEmail, message)
